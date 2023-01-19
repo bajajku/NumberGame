d8abb04d4c83ede5cfb2bea138d181f8efdae4a3
Rishabh Bajaj <bajajris@shernet.sheridancollege.ca>
2023-01-19 16:47:58 -0500

    we have generated 3 digits for user guess

--- NumberGame/MainPage.xaml.cs
+++ NumberGame/MainPage.xaml.cs
@@ -1,17 +1,33 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace NumberGame;
 
 public partial class MainPage : ContentPage
-{
+{   Random rGenetator= new Random();
+	int randomNumber; //creates a field variable
+
 	public MainPage()
 	{
 		InitializeComponent();
+		//generate a random 3 digit number
+		randomNumber=rGenetator.Next(100,1000);
 	}
 
     private void OnButtonclicked(object sender, EventArgs e)
     {
-		
+		//generate 3 digits 
+		int num1 = rGenetator.Next(0, 10);
+		int num2 = rGenetator.Next(0, 10);
+		int num3 = rGenetator.Next(0, 10);
+		//use them to generate corresponding image names
+		string s1 = $"n{num1}.png";
+        string s2 = $"n{num2}.png";
+        string s3 = $"n{num3}.png";
+
+        //use the generated image names to show images on main page
+
+
 
 
     }
